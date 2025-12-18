adeb372a8484ba044de67ae5ff624c02fe0a41e0
stefLesage <contact@webgiciel.com>
2025-12-18 10:33:51 +0100

    Replace AppUploads with AppLogger (file logger)

---
+++ src/WebgicielModules/AppLogger.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Webgiciel2\ModuleLog\WebgicielModules;
+
+class AppLogger
+{
+    private string $logDir;
+
+    public function __construct(string $projectDir)
+    {
+        $this->logDir = $projectDir . '/var/log/custom/';
+
+        if (!is_dir($this->logDir)) {
+            mkdir($this->logDir, 0777, true);
+        }
+    }
+
+    public function log(string $message, ?string $ip = null): void
+    {
+        $date = new \DateTime();
+
+        $filename = $this->getAvailableLogFilename(clone $date);
+
+        $logMessage = sprintf(
+            "[%s] %s%s\n",
+            $date->format('Y-m-d H:i:s'),
+            $ip ? $ip . ', ' : '',
+            $message
+        );
+
+        file_put_contents($filename, $logMessage, FILE_APPEND);
+    }
+
+    private function getAvailableLogFilename(\DateTime $date): string
+    {
+        $maxLines = 500; 
+
+        while (true) {
+            $logFilename = $this->logDir . 'log_' . $date->format('Y_m_d') . '.txt';
+
+            if (!file_exists($logFilename)) {
+                return $logFilename;
+            }
+
+            $lineCount = (int) exec(
+                'wc -l < ' . escapeshellarg($logFilename)
+            );
+
+            if ($lineCount < $maxLines) {
+                return $logFilename;
+            }
+
+            $date->modify('+1 day');
+        }
+    }
+}
